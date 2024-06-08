8b927e8bfa0a2de72a63a51facb127e01868b11a
Bartosz Bryniarski <brylka@jelenia.com>
2024-06-08 14:04:01 +0200

    Test trzeciej apki

---
+++ test3.py
@@ -0,0 +1,42 @@
+import datetime
+import unittest
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+class MyAplicationTests(unittest.TestCase):
+
+    def setUp(self):
+        options = UiAutomator2Options()
+        options.platform_name = 'Android'
+        options.device_name = 'emulator-5554'  # Zmień na nazwę Twojego urządzenia lub emulatora
+        options.app_package = 'com.example.myapplication'  # Zaktualizuj nazwę pakietu aplikacji
+        options.app_activity = '.MainActivity'  # Zaktualizuj nazwę aktywności, jeśli jest inna
+        options.automation_name = 'UiAutomator2'
+
+        self.driver = webdriver.Remote('http://localhost:4723', options=options)
+
+    def test_display_current_date(self):
+        expected_date = datetime.date.today().strftime("%Y-%m-%d")
+
+        simple_button = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//*[@content-desc='Prosty Przycisk']"))
+        )
+        simple_button.click()
+
+        date_text = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, f"//*[contains(@text, '{expected_date}')]"))
+        )
+
+        self.assertTrue(simple_button.is_displayed())
+        self.assertIn(expected_date, date_text.text)
+
+    def tearDown(self):
+        if self.driver:
+            self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
