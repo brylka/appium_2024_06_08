c32f42349b0f1eed149f1e42185a2707cc1044be
Bartosz Bryniarski <brylka@jelenia.com>
2024-06-08 12:49:00 +0200

    Dodanie testu do aplikacji Hello Android!

--- test1.py
+++ test1.py
@@ -9,13 +9,25 @@ from selenium.webdriver.common.by import By
 class MyAplicationTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        options = UiAutomator2Options()
+        options.platform_name = 'Android'
+        options.device_name = 'emulator-5554'  # Zmień na nazwę Twojego urządzenia lub emulatora
+        options.app_package = 'com.example.myapplication'  # Zaktualizuj nazwę pakietu aplikacji
+        options.app_activity = '.MainActivity'  # Zaktualizuj nazwę aktywności, jeśli jest inna
+        options.automation_name = 'UiAutomator2'
+
+        self.driver = webdriver.Remote('http://localhost:4723', options=options)
 
     def test_hello_android(self):
-        pass
+        greeting_text = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//android.widget.TextView[contains(@text, 'Hello Android!')]"))
+        )
+        self.assertIn('Hello Android!', greeting_text.text)
 
     def tearDown(self):
-        pass
+        if self.driver:
+            self.driver.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
