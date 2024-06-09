77f0e7781e6fe94d507ad9d237756996c5f89144
Bartosz Bryniarski <brylka@jelenia.com>
2024-06-09 09:32:39 +0200

    Test kalkulator 2+2

--- test_calc1.py
+++ test_calc1.py
@@ -19,20 +19,47 @@ class MyAplicationTests(unittest.TestCase):
         self.driver = webdriver.Remote('http://localhost:4723', options=options)
 
     def test_2_plus_2(self):
-        button_2 = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//*[@content-desc='Przycisk 2']"))
-        )
-        button_2.click()
+        #button_2 = WebDriverWait(self.driver, 10).until(
+        #    EC.presence_of_element_located((By.XPATH, "//*[@content-desc='Przycisk 2']"))
+        #)
+        #button_2.click()
 
         button_2 = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Przycisk 2'))
         )
         button_2.click()
 
+        button_plus = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Przycisk +'))
+        )
+        button_plus.click()
 
-    #def tearDown(self):
-    #    if self.driver:
-    #        self.driver.quit()
+        button_2 = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Przycisk 2'))
+        )
+        button_2.click()
+
+        button_equal = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Przycisk ='))
+        )
+        button_equal.click()
+
+        result_text = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, f"//*[contains(@text, 'Wynik:')]"))
+        )
+
+        result_correct = 4.0
+        result = float(result_text.text.split()[1])
+        self.assertEqual(result, result_correct)
+
+        result_correct_str = 'Wynik: 4.0'
+        result_str = result_text.text
+        self.assertEqual(result_correct_str, result_str)
+
+
+    def tearDown(self):
+        if self.driver:
+            self.driver.quit()
 
 
 if __name__ == '__main__':
